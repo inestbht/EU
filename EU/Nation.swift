d416dfbb9f1de1f81df6bb69e2e4ceeb11d9457d
Samuel Pena <inestbht@gmail.com>
2022-06-06 14:36:24 -0500

    COMPLETED Challenge - Ch. 5.6 - Structs - Modify the EU Challenge project

---
+++ EU/Nation.swift
@@ -0,0 +1,15 @@
+//
+//  Nation.swift
+//  EU
+//
+//  Created by Samuel Pena on 6/6/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import Foundation
+
+struct Nation {
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
