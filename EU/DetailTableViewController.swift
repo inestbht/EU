cc70544a04d632205e6814f7c1fdd4fcc7067ead
Samuel Pena <inestbht@gmail.com>
2022-06-05 23:17:49 -0500

    COMPLETED Challenge - Ch. 5.3 - Working with Multiple View Controllers, Segues, and Using a Static Table View Controller

---
+++ EU/DetailTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailTableViewController.swift
+//  EU
+//
+//  Created by Samuel Pena on 6/5/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var countryField: UITextField!
+    @IBOutlet weak var capitalField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
